2aa3d30193ff9b4298bb633910cabb94d683dbe8
wangwanying <wangwanying@didiglobal.com>
2021-01-10 16:26:55 +0800

    udpate: 完善Button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,12 +1,38 @@
 /** UI组件源文件，包含整个组件的内容和逻辑 */
 import React from "react";
+import * as PropTypes from 'prop-types';
 import './index.less';
-export interface ButtonProps {
-    size?: 'small' | "big" | "default";
-}
-export interface ButtonState {}
-export default class Button extends React.Component<ButtonProps, ButtonState> {
+
+const ButtonTypes = ["default","primary"];
+type ButtonType = (typeof ButtonTypes)[number];
+const ButtonSizes = ["large","small","default"];
+type ButtonSize = (typeof ButtonSizes)[number];
+export interface IButtonProps {
+    size?: ButtonSize;
+    type?: ButtonType;
+    children?: React.ReactNode;
+};
+export interface IButtonState {};
+// class 写法
+export default class Button extends React.Component<IButtonProps, IButtonState> {
+    static defaultProps: {
+        // default: boolean;
+    }
+    static propTypes: {
+        type: PropTypes.Requireable<string>;
+        size: PropTypes.Requireable<"small" | "default" | "large">;
+    }
     render() {
-        return (<div className="wrap"><button className="test">测试ing</button></div>);
+        const { children, type } = this.props;
+        return <button className={`coment-btn coment-btn-${type}`}>{children}</button>;
     }
 }
+// hook 写法
+// const Button: React.FunctionComponent<IButtonProps> = (props) => {
+//     const { children ,type = 'default'} = props;
+//     return <button className={type}>{children}</button>;
+// };
+
+// 使用hook的优势 : 
+// 简化原来写在生命周期中复杂的代码块，
+// 完全拥抱函数 
